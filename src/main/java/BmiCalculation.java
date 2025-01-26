0ba62b98f824a61514d04ef44b5dd5d9a5b4ecb2
Gábor Bábik <gaborbabik@gmail.com>
2025-01-26 18:35:42 +0100

    fixed if else structure

--- src/main/java/BmiCalculation.java
+++ src/main/java/BmiCalculation.java
@@ -4,26 +4,32 @@ public class BmiCalculation {
     static double result;
 
     // BMI számítása
-    static void bmiCalculationMethod(int weight, double height) {
-        result = weight / (height*height);
-        System.out.printf("Az ön BMI indexe: %.2f \n", result);
+    static void calculateBmi(int weight, double height) {
+        if (weight > 0 && height > 0) {
+            result = weight / (height * height);
+            System.out.printf("Az ön BMI indexe: %.2f \n", result);
+            evaluateBmi(result);
+        } else {
+            System.out.println("Negatív számot adtál meg!");
+            return;
+        }
     }
 
     // Eredmény kiértékelése
-    static void bmiEvaluation(double result) {
+    static void evaluateBmi(double result) {
         if (result<16) {
             System.out.println("Súlyos soványság");
-        } else if (result >= 16 & result <= 16.99) {
+        } else if (result >= 16 && result < 17) {
             System.out.println("Mérsékelt soványság");
-        } else if (result >= 17 & result <= 18.49) {
+        } else if (result >= 17 && result < 18.5) {
             System.out.println("Enyhe soványság");
-        } else if (result >= 18.5 & result <= 24.99) {
+        } else if (result >= 18.5 && result < 25) {
             System.out.println("Normális testsúly");
-        } else if (result >= 25 & result <= 29.99) {
+        } else if (result >= 25 && result < 30) {
             System.out.println("Túlsúlyos");
-        } else if (result >= 30 & result <= 34.99) {
+        } else if (result >= 30 && result < 35) {
             System.out.println("I. fokú elhízás");
-        } else if (result >= 35 & result <= 39.99) {
+        } else if (result >= 35 && result < 40) {
             System.out.println("II. fokú elhízás");
         } else if (result >= 40) {
             System.out.println("III. fokú (súlyos) elhízás");
@@ -39,8 +45,7 @@ public class BmiCalculation {
         int weight = scanner.nextInt();
         double height = scanner.nextDouble();
 
-        bmiCalculationMethod(weight, height);
-        bmiEvaluation(result);
+        calculateBmi(weight, height);
 
     }
 }
